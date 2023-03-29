6bf51405f421b71228de4afcbd73531fab999f6e
fcaumartin <fcaumartin@gmail.com>
2023-03-29 10:31:14 +0200

    refactor

--- api/index.php
+++ api/index.php
@@ -4,8 +4,8 @@
 ##################################################
 const DATABASE_HOST = "127.0.0.1";
 const DATABASE_NAME = "hotel";
-const DATABASE_USER = "dbuser";
-const DATABASE_PASSWORD = "fakefake";
+const DATABASE_USER = "admin";
+const DATABASE_PASSWORD = "Afpa1234";
 ##################################################
 
 $db = new PDO('mysql:host=' . DATABASE_HOST . ';dbname='. DATABASE_NAME .';charset=utf8mb4', DATABASE_USER, DATABASE_PASSWORD);
@@ -19,78 +19,38 @@ $uri_element = explode("/", $uri);
 $table = substr($uri_element[2], 0, -1);
 $id = $uri_element[3];
 
-
-$query = "";
 $data = null;
-
 switch($rm) {
     case "GET":
-        $query = "select * from $table";
-        if ($id) {
-            $query .= " where id=$id";
-        }
+        if ($id)
+            $data = get_item($db, $table, $id);
+        else
+            $data = get_items($db, $table);
     break;
 
     case "POST":
-        $json = file_get_contents('php://input', true);
-        $data = json_decode($json);
-        $props = get_object_vars($data);
-        $values = array_values($props);
-        $names = array_keys($props);
-        // var_dump($values);
-        $query = "insert into $table ";
-        $query .= "(" . implode(",", $names) . ")";
-        $query .= " values (" . implode(",", array_fill(0, count($names), '?')) . ")"; 
-        // var_dump($query);
-        // die;
-        $prepared_query = $db->prepare($query);
-        $prepared_query->execute($values);
-        $id = $db->lastInsertId();
-        $query = "select * from $table where id=$id";
+        $data = post_item($db, $table);
     break;
     
     case "PUT":
-        $json = file_get_contents('php://input', true);
-        $data = json_decode($json);
-        $props = get_object_vars($data);
-        $values = array_values($props);
-        $names = array_keys($props);
-        $set_names = array_map(fn($v): string => $v."=?", $names);
-        // var_dump($values);
-        $query = "update $table ";
-        $query .= "set " . implode(",", $set_names);
-        $query .= " where id=?";
-        array_push($values, $id);
-        // var_dump($query);
-        // die;
-        $prepared_query = $db->prepare($query);
-        $prepared_query->execute($values);
-        // $id = $db->lastInsertId();
-        $query = "select * from $table where id=$id";
+        $data = put_item($db, $table, $id);
     break;
 
     case "DELETE":
-        $query = "delete from $table";
-        $query .= " where id=$id";
+        delete_item($db, $table, $id);
     break;
 }
 
-
-    $request = $db->query($query);
-    if ($id) {
-        $result = $request->fetch(PDO::FETCH_OBJ);
-    }
-    else {
-        $result = $request->fetchAll(PDO::FETCH_OBJ);
-    }
-    echo json_encode($result);
+if ($data)
+    echo json_encode($data);
+    
 
 
 #####################################################
 # Simple CRUD functions
 #####################################################
 
-function get_ressources($db, $table) {
+function get_items($db, $table) {
 
     $prepared_query = $db->prepare("select * from $table");
     $result = $prepared_query->execute();
@@ -98,23 +58,56 @@ function get_ressources($db, $table) {
     return $result->fetchAll(PDO::FETCH_OBJ);
 }
 
-function get_ressource($db, $table, $id) {
+function get_item($db, $table, $id) {
 
     $prepared_query = $db->prepare("select * from $table where id=?");
-    $result = $prepared_query->execute($id);
+    $result = $prepared_query->execute([$id]);
 
     return $result->fetch(PDO::FETCH_OBJ);
 }
 
 
-function post_ressource($db, $table) {
+function post_item($db, $table) {
+    $json = file_get_contents('php://input', true);
+    $props = get_object_vars(json_decode($json));
+    $values = array_values($props);
+    $names = array_keys($props);
+    $query = "insert into $table ";
+    $query .= "(" . implode(",", $names) . ")";
+    $query .= " values (" . implode(",", array_fill(0, count($names), '?')) . ")"; 
+    // var_dump($query);
+    // die;
+    $prepared_query = $db->prepare($query);
+    $prepared_query->execute($values);
+    $id = $db->lastInsertId();
 
+    return get_item($db, $table, $id);
 }
 
-function put_ressource($db, $table, $id) {
-
+function put_item($db, $table, $id) {
+    $json = file_get_contents('php://input', true);
+    $data = json_decode($json);
+    $props = get_object_vars($data);
+    $values = array_values($props);
+    $names = array_keys($props);
+    $set_names = array_map(fn($v): string => $v."=?", $names);
+    // var_dump($values);
+    $query = "update $table ";
+    $query .= "set " . implode(",", $set_names);
+    $query .= " where id=?";
+    array_push($values, $id);
+    // var_dump($query);
+    // die;
+    $prepared_query = $db->prepare($query);
+    $prepared_query->execute($values);
+    
+    return get_item($db, $table, $id);
 }
 
-function delete_ressource($db, $table, $id) {
+function delete_item($db, $table, $id) {
 
+    $prepared_query = $db->prepare("delete from $table where id=?");
+    $result = $prepared_query->execute($id);
+
+    return "";
 }
